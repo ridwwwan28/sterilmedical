0700e6030d8cd09aca9c981b272e9b572b91a5f1
ridwwwan28 <muhammad.ridwan0590@gmail.com>
2026-07-08 12:05:52 +0700

    feature:install filament untuk admin panel/CMS

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'superadmin'])->default('admin');
+            $table->boolean('is_active')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
